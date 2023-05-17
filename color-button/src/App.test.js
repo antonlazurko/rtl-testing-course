10c2b3578b4464fe3b8d0a0e317fdc4eb87908c2
Anton <antla@ciklum.com>
2023-05-17 09:10:59 +0300

    add new test

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -6,3 +6,8 @@ test('renders learn react link', () => {
   const linkElement = screen.getByText(/learn react/i);
   expect(linkElement).toBeInTheDocument();
 });
+test('renders learn react link with text content', () => {
+  render(<App />);
+  const linkElement = screen.getByText(/learn react/i);
+  expect(linkElement.textContent).toBe('Learn React');
+});
