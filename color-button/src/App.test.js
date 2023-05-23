76f580f53e7844027ded3fe66ce98482281f26d9
Anton <antla@ciklum.com>
2023-05-23 09:42:25 +0300

    test transform function

--- color-button/src/App.test.js
+++ color-button/src/App.test.js
@@ -1,5 +1,7 @@
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
+import { replaceCamelWithSpaces } from './App';
+
 
 test('init color', () => {
   render(<App />);
@@ -33,3 +35,15 @@ test('button disabled end enable after checkbox checked', () => {
   expect(button).toBeEnabled()
   expect(button).toHaveStyle('background-color: red')
 });
+
+describe('spaces before camel-case capital letters', () => {
+  test('works for inner capital letters', () => {
+    expect(replaceCamelWithSpaces('Red')).toBe('Red')
+  })
+  test('works for one inner capital letters', () => {
+    expect(replaceCamelWithSpaces('MidnightBlue')).toBe('Midnight Blue')
+  })
+  test('works for one multuply capital letters', () => {
+    expect(replaceCamelWithSpaces('MediumViolateRed')).toBe('Medium Violate Red')
+  })
+})
