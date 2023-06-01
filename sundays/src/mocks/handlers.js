8e522cfc6dcaee341098c237df6e0a2ff65c343c
Anton <antla@ciklum.com>
2023-06-01 09:39:30 +0300

    add msw and config server

---
+++ sundays/src/mocks/handlers.js
@@ -0,0 +1,15 @@
+import { rest } from 'msw'
+export const handlers = [
+    rest.get('http://localhost:3030/scoops', (req, res, ctx) => {
+        return res(
+            ctx.status(200),
+            ctx.json([
+                {
+                    name: 'chocolate', imagePath: '/images/chocolate.png'
+                },{
+                    name: 'vanilla', imagePath: '/images/vanilla.png'
+                }
+            ])
+        )
+    })
+]
